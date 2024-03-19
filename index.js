ebce84f0cf5586f71f26436761ab4158ffcdd7b0
Atiq Ur Rehman <128805823+AtiqDevExpert@users.noreply.github.com>
2024-03-19 14:23:59 +0500

    updated code

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ const PORT = 8000;
 
 const userRouter = require("./routes/user");
 const { connectMongoDB } = require("./connection");
-const { logReqRes } = require("./middleware");
 
 const dbUrl =
   "mongodb+srv://atiqurrehman01m:tlNn3dkbCzc7jcKn@cluster0.qgeyfgv.mongodb.net/crudusers";
-const fileName = "log.txt";
+
 connectMongoDB(dbUrl);
 
 // encoded body urlencoded and Json
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ extended: false }));
-// custom middleware
-app.use(logReqRes(fileName));
 // Routes
 
 app.use("/api/users", userRouter);
